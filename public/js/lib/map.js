7216db1ca01281312810bdc035db7dce8591ad47
andrew-wood <andrew.wood@mac.com>
2018-06-15 14:23:37 +0100

    maps changes

--- public/js/lib/map.js
+++ public/js/lib/map.js
@@ -4,4 +4,37 @@ function initMap() {
     center: {lat: 55.9533, lng: -3.1883},
     zoom: 17
   });
+
+  updateWithGeoLocation();
+  getLocalPerformers();
+}
+
+function updateWithGeoLocation(){
+    navigator.geolocation.getCurrentPosition(function(position) {
+        console.log(position.coords.latitude, position.coords.longitude);
+      });
+
+}
+
+function getLocalPerformers(){
+    jQuery.getJSON( "/js/lib/buskerLocations.json" , addPerformersToMap) ;
+    
+}
+
+function addPerformersToMap(data){
+    data.forEach(busker => {addBuskerToMap(busker);
+        
+    });
+
+}
+
+function addBuskerToMap(busker){
+    var marker = new google.maps.Marker({position: busker.pitch.location, map: map, title:busker.name});
+    var infowindow = new google.maps.InfoWindow({
+        content: busker.busker  + " - " + busker.rating + " - " + busker.description
+      });
+      marker.addListener('click', function() {
+        infowindow.open(map, marker);
+      });
+    console.log(busker);
 }
